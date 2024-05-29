707cc8131721195a261bba6bef3a55e84d98a350
financial investor <financilainvestor2020@gmail.com>
2024-05-29 18:08:07 +0430

    third commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const person = {
+    name: "Mohammadreza",
+    lastName: "Ghanbari",
+    age: 26,
+    phone: "+989302504556",
+    email: "financialinvestor2020@gmail.com",
+    prop: {
+        weight: 88,
+        height: 185
+    },
+}
+
+console.log("My name is " + person.name + " " + person.lastName)
+console.log("my phonenumber is" + person.phone + " " + "my email is" + person.email)
+console.log("My weight is " + person.prop.weight + " " +"my height is " + person.prop.height + " " + "my age is " + person.age)
